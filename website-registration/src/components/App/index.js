e885b9208ea71e0855a8fa25d6ff32195ef9f840
Sasha <o.stolitenko@temabit.com>
2021-11-09 13:36:52 +0200

    Using react router guards middleware

--- website-registration/src/components/App/index.js
+++ website-registration/src/components/App/index.js
@@ -1,56 +1,73 @@
 import { useState } from 'react';
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
 import { Link, Route, Switch } from 'react-router-dom';
-import GuardedRoute from '../GuardedRoute';
+import { GuardProvider, GuardedRoute } from 'react-router-guards';
 
-import { HomePage, UnprotectedPage, ProtectedPageOne, ProtectedPageTwo } from '../../pages';
+import { HomePage, UnprotectedPage, ProtectedPageOne } from '../../pages';
+import LogIn from '../LogIn';
 
 import './style.css';
-import LogIn from '../LogIn';
+
+const history = createBrowserHistory();
 
 const App = () => { 
   const [isRegistered, setIsRegistered] = useState(false);
 
+  const requireLogin = (to, from, next) => {
+    if (to.meta.auth) {
+      console.log("auth true");
+      if (isRegistered) {
+        next();
+      }
+      next.redirect('/');
+    } else {
+      next();
+    }
+  };
+
   return (
-    <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid px-5">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto me-lg-5">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            {isRegistered && 
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/protected-one">Protected page</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/protected-two">Protected page</Link>
-                </li>
-              </>
-            }
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/unprotected">Unprotected page</Link>
-            </li>
-          </ul> 
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Log in
+    <Router history={history}>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid px-5">
+          <Link className="navbar-brand" to="/">Navbar</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto me-lg-5">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              </li>
+              {isRegistered && 
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to={() => history.push('/protected-one')}>Protected page</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="/protected-two">Protected page</Link>
+                  </li>
+                </>
+              }
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/unprotected">Unprotected page</Link>
+              </li>
+            </ul> 
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Log in
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
-    <LogIn logInFunc={() => setIsRegistered(true)} logOutFunc={() => setIsRegistered(false)}/>
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <GuardedRoute path='/protected-one' component={ProtectedPageOne} auth={isRegistered}/>
-      <GuardedRoute path='/protected-two' component={ProtectedPageTwo} auth={isRegistered}/>
-      <Route path='/unprotected' component={UnprotectedPage}/>
-    </Switch>
-    </>
+      </nav>
+      <LogIn logInFunc={() => setIsRegistered(true)} logOutFunc={() => setIsRegistered(false)}/>
+      <GuardProvider guards={[requireLogin]}>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/unprotected' component={UnprotectedPage}/>
+          <GuardedRoute path='/protected-one' component={ProtectedPageOne} meta={{auth: true}}/>
+        </Switch>
+      </GuardProvider>
+    </Router>
   );
 }
 
--- website-registration/src/index.js
+++ website-registration/src/index.js
@@ -5,6 +5,6 @@ import App from './components/App';
 ReactDOM.render(
   <BrowserRouter>
     <App/>
-  </BrowserRouter>
+  </BrowserRouter>  
   ,document.getElementById('root')
 );
